45b1dcef245699b3723714125a5d03309da5ce8f
nlinkova <nlinkova@finlab.ru>
2024-12-18 16:25:04 +0300

    add custom class applier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
-import './index.scss';
+import { classNames } from './helpers/classnames/classnames';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={classNames('app',
+      { hovered: true, selected: true },
+      [theme, 'cls2', 'cls3']
+    )}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to="/">Main Page</Link>
       <Link to="/about">About Page</Link>
       <Suspense fallback={<div>Loading...</div>}>
