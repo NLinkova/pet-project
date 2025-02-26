5d7bc03642b202c20389d6d2a7946659bfd306c6
nlinkova <nlinkova@finlab.ru>
2025-02-26 18:17:38 +0300

    add jest

--- src/shared/lib/classnames/classnames.ts
+++ src/shared/lib/classnames/classnames.ts
@@ -1,10 +1,10 @@
 type Mods = Record<string, boolean | string>;
-export function classNames(cls: string, mods?: Mods, additional?: string[]): string {
+export function classNames(cls: string, mods: Mods, additional: string[]): string {
   return [
     cls,
     ...additional.filter(Boolean), // filter if classname is undefined or null
     ...Object.entries(mods)
-      .filter(([cls, value]) => Boolean(value))
-      .map(([cls]) => cls),
+      .filter(([_, value]) => Boolean(value))
+      .map(([className]) => className),
   ].join(' ');
 }
