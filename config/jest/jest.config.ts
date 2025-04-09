19939d684be6aeba148a46562e7a7ad91ad14f74
nlinkova <nlinkova@finlab.ru>
2025-04-09 18:18:19 +0500

    add react testing

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -3,22 +3,14 @@
  * https://jestjs.io/docs/configuration
  */
 
+import path from 'path';
+
 export default {
-  // Automatically clear mock calls, instances and results before every test
   clearMocks: true,
-  // The test environment that will be used for testing
   testEnvironment: 'jsdom',
-  // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: [
     '\\\\node_modules\\\\',
   ],
-
-  // An array of directory names to be searched recursively up from the requiring module's location
-  moduleDirectories: [
-    'node_modules',
-  ],
-
-  // An array of file extensions your modules use
   moduleFileExtensions: [
     'js',
     'jsx',
@@ -27,14 +19,22 @@ export default {
     'json',
     'node',
   ],
-
-  // The root directory that Jest should scan for tests and modules within
-  rootDir: '../../',
-
-  // The glob patterns Jest uses to detect test files
+  moduleDirectories: [
+    'node_modules',
+  ],
+  modulePaths: [
+    '<rootDir>src',
+  ],
   testMatch: [
+    // Обнаружил разницу между МАК ОС и ВИНДОУС!!!
     '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
   ],
+  rootDir: '../../',
+  setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.s?css$': 'identity-obj-proxy',
+    '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+  },
 
   // All imported modules in your tests should be mocked automatically
   // automock: false,
